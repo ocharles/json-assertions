0d4a7a2a14736e2c7e827618c4e6042a7ce09876
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:45:11 +0000

    Regenerate nix expressions

--- default.nix
+++ default.nix
@@ -1,16 +1,14 @@
-{}:
-with import <nixpkgs> {};
-let
-  inherit (haskellPackages) aeson ariadne attoparsec
-    cabal cabalInstall doctest filepath genericDeriving lens lensAeson
-    semigroups simpleReflect snap tasty tastyHunit tastySmallcheck tastyQuickcheck tastyAntXml
-    text unorderedContainers utf8String vector webRoutesBoomerang free
-    quickcheckInstances indexed indexedFree;
-
-in cabal.mkDerivation (self: {
+{ mkDerivation, aeson, base, indexed, indexed-free, lens
+, lens-aeson, stdenv, text
+}:
+mkDerivation {
   pname = "json-assertions";
-  version = "0.1.0";
+  version = "1.0.5";
   src = ./.;
-  buildDepends = [ aeson indexed indexedFree lens lensAeson free text ];
-  buildTools = [ cabalInstall ];
-})
+  buildDepends = [
+    aeson base indexed indexed-free lens lens-aeson text
+  ];
+  homepage = "http://github.com/ocharles/json-assertions.git";
+  description = "Test that your (Aeson) JSON encoding matches your expectations";
+  license = stdenv.lib.licenses.bsd3;
+}
